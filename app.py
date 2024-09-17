7274c127eaa151a1ae2d7d72975f8b33653aeeaf
queen <kafikoa77@gmail.com>
2024-09-17 17:21:08 +0100

    added Project class and json

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import json
 from dotenv import load_dotenv
-
+from models import Project  # Import the Project class
 load_dotenv()
 
 app = Flask(__name__)
 
+def load_projects():
+    with open('projects.json') as f:
+        project_data = json.load(f)
+        projects = [Project(**proj) for proj in project_data]
+    return projects
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    projects = load_projects()
+    latest_projects = sorted(projects, key=lambda x: x.date, reverse=True)[:6] 
+    return render_template('index.html', projects=latest_projects)
+
 
 @app.route('/about')
 def about():
@@ -22,11 +31,13 @@ def contact():
 def projects():
     return render_template('projects.html')
 
-@app.route('/')
-def index():
+@app.route('/project/<int:project_id>')
+def project_details(project_id):
     projects = load_projects()
-    latest_projects = sorted(projects, key=lambda x: x.date, reverse=True)[:6] 
-    return render_template('index.html', projects=latest_projects)
+    project = next((proj for proj in projects if proj.id == project_id), None)
+    if project is None:
+        return "Project not found", 404
+    return render_template('project_details.html', project=project)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,10 @@
+#models.py or inside app.py
+class Project:
+    def __init__(self, id, title, module, description, thumbnail, date, technologies):
+        self.id = id
+        self.title = title
+        self.module = module
+        self.description = description
+        self.thumbnail = thumbnail
+        self.date = date
+        self.technologies = technologies
